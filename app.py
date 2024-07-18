15d5d5ba73947962b2b7f4bd898b1c4e1642d8d2
“hxc338” <hxc338@student.bham.ac.uk>
2024-07-19 00:12:13 +0100

    updated base and index html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+# from forms import LoginForm, RegistrationForm
 import sys
 
 app = Flask(__name__)
@@ -7,6 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         flash(f'Registration for {form.username.data} received', 'success')
+#         return redirect(url_for('index'))
+#     return render_template('registration.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
